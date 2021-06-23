c66c9dc67a7dc98076d55d7cda94812450fe4894
xTeKc <bcd3v@pm.me>
2021-06-23 16:39:07 -0400

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,13 @@
+require("@nomiclabs/hardhat-waffle");
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
