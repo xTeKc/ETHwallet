cc63d762ccdf121d811c8ddb5dc8053734e81bf6
xTeKc <bcd3v@pm.me>
2021-06-22 23:15:55 -0400

    update

--- qkdapp/src/contracts/Token.sol
+++ qkdapp/src/contracts/Token.sol
@@ -1,5 +1,8 @@
 pragma solidity 0.8.0;
 
 contract Gash {
-    
+    string public name = Gash;
+    string public symbol = GSH;
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
 }
