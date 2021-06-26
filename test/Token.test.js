2394bc8e5dbf4e06d1dbd416270eb26d8f70c131
xTeKc <bcd3v@pm.me>
2021-06-25 21:55:20 -0400

    rearrange contents

--- test/Token.test.js
+++ test/Token.test.js
@@ -4,7 +4,6 @@ describe('Token contract', () => {
 	it('Should show the token Name & Symbol', async () => {
 		const Token = await ethers.getContractFactory('Token');
 		const token = await Token.deploy('Token Deployed!!');
-		await token.deployed();
 
 		expect(await token.NameAndSymbol().to.equal('bStash', 'BSTH'));
 		
