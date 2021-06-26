6dc3b2588e82a81a7052b0d99a6ce5796848602b
xTeKc <bcd3v@pm.me>
2021-06-25 22:28:40 -0400

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,10 +8,32 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.0",
-};
-
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    // rinkeby: {
+    //   url: "https://eth-mainnet.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+    //   accounts: [privateKey1, privateKey2, ...]
+    // }
+  },
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+}
