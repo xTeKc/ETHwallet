721195374fbe79cbe957d78391a9657d316c5ba7
xTeKc <bcd3v@pm.me>
2021-06-25 21:36:46 -0400

    edit

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,7 +1,6 @@
 const { expect } = require('chai');
-const { ethers } = require('ethers');
 
-describe('Token Info', () => {
+describe('Token contract', () => {
 	it('Should show the token Name & Symbol', async () => {
 		const Token = await ethers.getContractFactory('Token');
 		const token = await Token.deploy('Token Deployed!!');
