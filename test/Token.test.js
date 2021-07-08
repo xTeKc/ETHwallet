ef50ba9ce110a85f860238a0ca6dd6fbfed82472
xTeKc <bcd3v@pm.me>
2021-07-07 22:13:45 -0400

    let

--- test/Token.test.js
+++ test/Token.test.js
@@ -2,6 +2,10 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
 describe('Token contract', () => {
+
+	let owner;
+	let Token;
+
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token');
 		const [owner] = await ethers.getSigners();
@@ -10,7 +14,6 @@ describe('Token contract', () => {
 	it('Should show the token Name & Symbol', async () => {
 		const token = await Token.deploy('Token Deployed!!');
 		const ownerBalance = await bStash.balanceOf(owner.address);
-
 		expect(await bStash.totalSupply()).to.eq(ownerBalance);
 		expect(await token.NameAndSymbol().to.equal('bStash', 'BSTH'));
 		
