4a8ce7b68ab4dd23afb3b9f4b0d30349847151f2
xTeKc <bcd3v@pm.me>
2021-07-07 22:14:44 -0400

    refactor

--- test/Token.test.js
+++ test/Token.test.js
@@ -11,13 +11,6 @@ describe('Token contract', () => {
 		const [owner] = await ethers.getSigners();
 	}) 
 
-	it('Should show the token Name & Symbol', async () => {
-		const token = await Token.deploy('Token Deployed!!');
-		const ownerBalance = await bStash.balanceOf(owner.address);
-		expect(await bStash.totalSupply()).to.eq(ownerBalance);
-		expect(await token.NameAndSymbol().to.equal('bStash', 'BSTH'));
-		
-	})
 
 
 
