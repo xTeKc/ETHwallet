d2a761475a13d4d7bd12468906f7b0dd39eb2978
xTeKc <bcd3v@pm.me>
2021-07-07 22:11:38 -0400

    modify test

--- test/Token.test.js
+++ test/Token.test.js
@@ -2,9 +2,12 @@ const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
 describe('Token contract', () => {
-	it('Should show the token Name & Symbol', async () => {
-		const [owner] = await ethers.getSigners();
+	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token');
+		const [owner] = await ethers.getSigners();
+	}) 
+
+	it('Should show the token Name & Symbol', async () => {
 		const token = await Token.deploy('Token Deployed!!');
 		const ownerBalance = await bStash.balanceOf(owner.address);
 
